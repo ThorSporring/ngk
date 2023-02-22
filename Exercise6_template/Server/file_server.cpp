93776a36e560420480840ebd1cd2fe9adbf3bb2b
Thor Sporring <thor@sporring.eu>
2023-02-22 21:52:40 +0100

    lege rundt med alt muligt

--- Exercise6_template/Server/file_server.cpp
+++ Exercise6_template/Server/file_server.cpp
@@ -15,7 +15,7 @@ Extended to support file server!
 #include <netinet/in.h>
 #include "iknlib.h"
 
-#define STRBUFSIZE 256
+#define BUFSIZE_TX 1000
 
 void error(const char *msg)
 {
@@ -42,8 +42,49 @@ int main(int argc, char *argv[])
 {
 	printf("Starting server...\n");
 
+	int sockfd, newsockfd, portno;
+	
+	size_t numBytes;
+	FILE * fp;
+
+	socklen_t clilen;
+	char transferBuffer[BUFSIZE_TX];
+	char fileName[256];
+	struct sockaddr_in serv_addr, cli_addr;
+	int n;
+	
+	if (argc < 2) 
+		error("ERROR usage: port");
 
 
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0) 
+		error("ERROR opening socket");
 
+	printf("Binding...\n");
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	portno = atoi(argv[1]);
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = INADDR_ANY;
+	serv_addr.sin_port = htons(portno);
+	if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
+		error("ERROR on binding");
+
+	printf("Listen...\n");
+	listen(sockfd,5);
+
+	clilen = sizeof(cli_addr);
+
+	printf("Accept...\n");
+	newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+
+	if (newsockfd < 0) error("ERROR on accept");
+	else printf("Accepted\n");
+
+	bzero(transferBuffer,sizeof(transferBuffer));
+	n = read(newsockfd,transferBuffer,sizeof(transferBuffer));
+
+	if (n < 0) error("ERROR reading from socket");
+	printf("Filename: %s\n",(char*)transferBuffer);
 	return 0; 
 }
